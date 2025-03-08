a1f6a19b57cc0c89e6a2c70613dcebd1dd0272bd
Harsh  Bhoir <harshbhoir1302@gmail.com>
2025-03-09 00:59:01 +0530

    added leading in Celebration block 1

--- src/app/celebration/CelebrationsBlock1.jsx
+++ src/app/celebration/CelebrationsBlock1.jsx
@@ -4,7 +4,7 @@ const CelebrationsBlock1 = () => {
   return (
     <div className=" mx-6 md:mx-12  ">
       <div className=" border border-[#A52A2A] rounded-2xl p-6 mt-6 mb-2">
-        <h3 className="font-crimson  text-[27px] text-[#A52A2A]  font-bold leading-5">
+        <h3 className="font-crimson  text-[27px] text-[#A52A2A]  font-bold leading-7">
           Consolidated Community Development Initiatives -
           <span>Across 12 Villages</span>
         </h3>
